8688f9423df95501080ba7dcf40a9fcd96b1da1e
Julien Jesionek <julien.jesionek@gmail.com>
2022-09-29 21:53:19 +0200

    Habillage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home () {
                 <h2>Bienvenue !</h2>
             </div>
             <div className="home-body">
-                <p>Je suis Julien Jesionek, Développeur Web.</p>
-                <p>Vous visitez actuellement le site de présentation de mon profil.</p>
+                <p>Je suis Julien Jesionek, <strong>Développeur Web</strong>.</p>
+                <p>Vous visitez actuellement le site de <strong>présentation</strong> de mon profil.</p>
                 <p>Si vous êtes ici, c'est probablement parce que j'ai candidaté pour rejoindre votre entreprise ou que l'on vous a parlé de moi.</p>
                 <p>Dans tous les cas, cela vous intéressera de visiter les pages <NavLink exact="true" to="/aboutme">Qui suis-je?</NavLink> et <NavLink exact="true" to="/portfolio">Portfolio</NavLink>.</p>
             </div>
