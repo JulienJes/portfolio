0bb64aeac08f558022bfe6d0c664fabaa030ff2d
Julien Jesionek <julien.jesionek@gmail.com>
2022-10-18 17:15:22 +0200

    working onclick

--- src/components/events/onclick.js
+++ src/components/events/onclick.js
@@ -1,7 +1,7 @@
 function OnClick () {
     let answer = window.confirm("Ce lien mène vers un site externe, souhaitez-vous l'ouvrir dans un nouvel onglet?")
     if(answer === true) {
-        window.open(this.href, "target='_blank'", "rel='noopener noreferrer'");
+        window.open(this.href, "target='_blank' rel='noopener noreferrer'");
     }
 };
 
