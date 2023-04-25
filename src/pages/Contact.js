3858537ae79cd6b9ae4b99ad81bec8dee62903ce
Julien Jesionek <julien.jesionek@gmail.com>
2023-04-25 16:45:15 +0200

    htmlfor

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,19 +8,19 @@ function Contact() {
                 <p>Tous les champs sont obligatoires.</p>
                 <form action="https://formsubmit.co/julien.jesionek@gmail.com" method="POST">
                     <div className="field">
-                        <label for="name">Nom</label>
+                        <label htmlFor="name">Nom</label>
                         <input type="text" id="name" name="user_name" placeholder="Votre nom" required></input>
                     </div>
                     <div className="field">
-                        <label for="mail">Adresse mail</label>
+                        <label htmlFor="mail">Adresse mail</label>
                         <input type="email" id="mail" name="user_mail" placeholder="Votre adresse mail" required></input>
                     </div>
                     <div className="field">
-                        <label for="object">Sujet</label>
+                        <label htmlFor="object">Sujet</label>
                         <input type="text" id="object" name="user_object" placeholder="Sujet de votre message" required></input>
                     </div>
                     <div className="field">
-                        <label for="message">Message</label>
+                        <label htmlFor="message">Message</label>
                         <textarea id="message" name="user_message" placeholder="Votre message" required></textarea>
                     </div>
                     <div className="button">
